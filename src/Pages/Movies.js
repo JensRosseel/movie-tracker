879fe1e6bad006908d22d9b612477150caefa095
Jens Rosseel <slaythehorizon.games@gmail.com>
2022-01-15 23:36:07 +0100

    Add genre selector

--- src/Pages/Movies.js
+++ src/Pages/Movies.js
@@ -2,26 +2,39 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Card from '../components/Card/Card'
 import CustomPagination from '../components/Pagination/CustomPagination'
+import Genres from '../components/Genres'
+import useGenre from '../hooks/useGenre'
 
 const Movies = () => {
     const [page, setPage] = useState(1);
     const [content, setContent] = useState([]);
     const [numOfPages, setNumOfPages] = useState();
+    const [genres, setGenres] = useState([]);
+    const [selectedGenres, setSelectedGenres] = useState([]);
+    const genreforURL = useGenre(selectedGenres);
 
     const fetchMovies = async () => {
-        const { data } = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en_US&sort_by=popularity.desc&include_adult=false&page=${page}`);
+        const { data } = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`);
 
         setContent(data.results);
         setNumOfPages(data.total_pages);
     }
 
     useEffect(() => {
-        fetchMovies();
-    }, [page])
+        fetchMovies(); // eslint-disable-next-line 
+    }, [genreforURL, page]); 
 
     return (
         <>
             <div className="page">
+                <Genres 
+                    type='movie'
+                    genres={genres}
+                    setGenres={setGenres}
+                    selectedGenres={selectedGenres}
+                    setSelectedGenres={setSelectedGenres}
+                    setPage={setPage}
+                />
                 <div className="cards">
                     {
                         content && content.map((c) => (
