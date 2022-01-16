e3eba25fd57f1729b56c8fa28f8b3e193a39ac11
Jens Rosseel <slaythehorizon.games@gmail.com>
2022-01-16 20:54:55 +0100

    Add series page functionality

--- src/Pages/Series.js
+++ src/Pages/Series.js
@@ -1,10 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Card from '../components/Card/Card'
+import CustomPagination from '../components/Pagination/CustomPagination'
+import Genres from '../components/Genres'
+import useGenre from '../hooks/useGenre'
 
 const Series = () => {
+    const [page, setPage] = useState(1);
+    const [content, setContent] = useState([]);
+    const [numOfPages, setNumOfPages] = useState();
+    const [genres, setGenres] = useState([]);
+    const [selectedGenres, setSelectedGenres] = useState([]);
+    const genreforURL = useGenre(selectedGenres);
+
+    const fetchSeries = async () => {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`);
+
+        setContent(data.results);
+        setNumOfPages(data.total_pages);
+    }
+
+    useEffect(() => {
+        fetchSeries(); // eslint-disable-next-line 
+    }, [genreforURL, page]); 
+
     return (
-        <>
-            <span className="pageSeries">TV Series</span>
-        </>
+        <div className="page">
+                <Genres 
+                    type='tv'
+                    genres={genres}
+                    setGenres={setGenres}
+                    selectedGenres={selectedGenres}
+                    setSelectedGenres={setSelectedGenres}
+                    setPage={setPage}
+                />
+                <div className="cards">
+                    {
+                        content && content.map((c) => (
+                            <Card
+                                key={c.id} 
+                                id={c.id} 
+                                poster={c.poster_path} 
+                                title={c.title || c.name} 
+                                date={c.first_air_date || c.release_date}
+                                media_type='tv'
+                                vote_average={c.vote_average}
+                            />
+                        ))
+                    }
+                </div>
+                {numOfPages>1 && (
+                    <CustomPagination setPage={setPage} numOfPages={numOfPages} />
+                )}
+            </div>
     )
 }
 
