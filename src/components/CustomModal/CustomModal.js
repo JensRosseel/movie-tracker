ce14d630320b6b6bc6066300ddd11f6b5084118a
Jens Rosseel <slaythehorizon.games@gmail.com>
2022-01-17 02:46:48 +0100

    Remove console log from fetchData

--- src/components/CustomModal/CustomModal.js
+++ src/components/CustomModal/CustomModal.js
@@ -29,7 +29,6 @@ export default function CustomModal({ children, media_type, id }) {
   const fetchData = async () => {
         const { data } = await axios.get(`https://api.themoviedb.org/3/${media_type}/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`);
 
-        console.log(data);
         setContent(data);
   }
 
